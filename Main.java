6c67dfcdfb9cbf8b82bb17b47f1d03115efab8c5
Gustavo Lino <137111588+GutLuiz@users.noreply.github.com>
2023-10-19 11:05:44 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,74 +2,51 @@ package curso_programacao;
 import java.util.Locale;
 import java.util.Scanner;
 
-import util.Conta_Bancaria;
-
+import util.Banco;
 public class Main {
-	
-	
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		Conta_Bancaria x = new Conta_Bancaria();
+		Banco banco;
 		
-		   System.out.println("BEM VINDO!");
-		   System.out.println("faça sua escolha:" );
-		   System.out.println("1 = DEPOSITAR");
-		   System.out.println("2 = SACAR ");
-		   System.out.println("3 = SAIR");
-		   System.out.print("ESCOLHA: ");
-	        x.escolher1 = sc.nextInt();
-
-	        while (x.escolher1 != 3) {
-	        	if (x.escolher1 == 1) {
-	        		System.out.print("Quanto você quer depositar?");
-		            x.deposito = sc.nextDouble();
-		            
-		            System.out.printf("Seu saldo agora é: %.2f%n", x.dinheiro());
-		            x.saldo += x.deposito;
-		            
-		            System.out.print("Deseja repetir (s/n)? ");
-		            char resposta = sc.next().charAt(0);
-
-		            if (resposta != 's' && resposta != 'S') {
-		                break;
-		            }
-
-		           System.out.println("BEM VINDO!");
-		 		   System.out.println("faça sua escolha:" );
-		 		   System.out.println("1 = DEPOSITAR");
-		 		   System.out.println("2 = SACAR ");
-		 		   System.out.println("3 = SAIR");
-		 		   System.out.print("ESCOLHA: ");
-		           x.escolher1 = sc.nextInt();
-	        	}
-	        	else if(x.escolher1 == 2 && x.saldo > 0) {
-	        		System.out.print("Quanto você quer sacar?");
-		            x.saque = sc.nextDouble();
-		            
-		            System.out.printf("Seu saldo agora é: %.2f%n", x.sacar());
-		            x.saldo -= x.saque;
-		            
-		            System.out.print("Deseja repetir (s/n)? ");
-		            char resposta = sc.next().charAt(0);
-
-		            if (resposta != 's' && resposta != 'S') {
-		                break;
-		            }
-
-		           System.out.println("BEM VINDO!");
-		 		   System.out.println("faça sua escolha:" );
-		 		   System.out.println("1 = DEPOSITAR");
-		 		   System.out.println("2 = SACAR ");
-		 		   System.out.println("3 = SAIR");
-		 		   System.out.print("ESCOLHA: ");
-		           x.escolher1 = sc.nextInt();
-	        	}
-	            
-	        }
-	        
-
-	        System.out.println("OBRIGADO! Volte sempre.");
+		System.out.println("HELLO!");
+		System.out.println();
+		
+		System.out.print("Enter account number: ");
+		int number = sc.nextInt();
+		
+		System.out.print("Enter account holder: ");
+		String holder = sc.nextLine();
+		
+		System.out.print("Is there na initial deposit (y/n)?" );
+		char resposta = sc.next().charAt(0);
+		
+		if(resposta == 'y') {
+			System.out.print("Enter initial deposit value: ");
+			double valueInitial = sc.nextDouble();
+			 banco = new Banco( number, holder, valueInitial);
+		}
+		else {
+			banco = new Banco(number , holder);
+		}
+		
+		System.out.println();
+		System.out.println("Account data: ");
+		System.out.println(banco);
+		
+		System.out.println();
+		System.out.print("enter a deposit value: ");
+		double depositValue = sc.nextDouble();
+		banco.deposit(depositValue);
+		System.out.println("Upadate account data:");
+		System.out.println(banco);
+		
+		System.out.println();
+		System.out.print("enter a withdraw value: ");
+		double withdrawValue = sc.nextDouble();
+		banco.Saque(withdrawValue);
+		System.out.println("Upadate account data:");
+		System.out.println(banco);
 		
 		
 		
